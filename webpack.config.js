a1b7622591ef0d86b290f5a59ae47a75c205eb0b
MicroDreamIT <shahanur700@gmail.com>
2019-05-11 03:02:53 +0600

    first

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: "inline-source-map",
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: "output management"
+            template: "./src/index.html",
+            inlineSource: '.js$'
         })
     ],
     output: {
